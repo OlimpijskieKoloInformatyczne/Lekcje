26bac9f8929cfe54d2cb4f28a041cbd86c754579
OlimpijskieKoloInformatyczne <102910988+OlimpijskieKoloInformatyczne@users.noreply.github.com>
2023-02-13 18:36:16 +0100

    Add files via upload

--- set-size-iterator.cpp
+++ set-size-iterator.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
  set<int> grupy;
+ set<int>::iterator it; // wskaznik
+ 
  grupy.insert(20);
  grupy.insert(20);
  grupy.insert(10);
@@ -12,10 +14,18 @@ int main() {
  grupy.insert(20);
  grupy.insert(15);
 
- cout << grupy.size() << endl;
+ it = grupy.begin(); //wskaznik na pierwszy element
+ cout << *it << endl;
+ 
+ ++it; //wsskaznik na drugi
+ cout << *it << endl;
 
+ ++it; //wskaznik na 3
+ cout << *it << endl;
+ 
 
 
  
  return 0;
 }
+
